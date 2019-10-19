12cc2717699b22700d567d75980409a4987227c2
ewaugh13 <ewaugh13@yahoo.com>
2019-10-18 20:52:07 -0600

    Have a band saw with a moving blade

---
+++ SeriousPrototype/Assets/CustomAssets/Scripts/BandSawBladeMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BandSawBladeMovement : MonoBehaviour
+{
+    #region Instance Variables
+    [Tooltip("The start y position of the band saw blade")]
+    [SerializeField]
+    private float startYPosition = 0;
+    [Tooltip("The end y position of the band saw blade")]
+    [SerializeField]
+    private float topYPostion = 0;
+    [Tooltip("The smaller the time the faster the blade moves")]
+    [SerializeField]
+    private float timeToMove = .5f;
+    #endregion
+
+    #region Hidden Variables
+    private bool machineOn;
+    private bool moving;
+
+    private float xPosition;
+    private float zPosition;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        machineOn = false;
+        moving = false;
+
+        xPosition = this.gameObject.GetComponent<Transform>().localPosition.x;
+        zPosition = this.gameObject.GetComponent<Transform>().localPosition.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (machineOn)
+        {
+            float localPosY = this.gameObject.GetComponent<Transform>().localPosition.y;
+            if (!moving)
+            {
+                if (Mathf.Abs(localPosY - topYPostion) < 0.01)
+                {
+                    this.gameObject.GetComponent<Transform>().localPosition = new Vector3(xPosition, startYPosition, zPosition);
+                }
+                if (Mathf.Abs(localPosY - startYPosition) < 0.01)
+                {
+                    StartCoroutine(moveBlade(startYPosition, topYPostion, timeToMove));
+                    moving = true;
+                }
+            }
+        }
+    }
+
+    private IEnumerator moveBlade(float start, float end, float time)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            float yValue = Mathf.Lerp(start, end, elapsedTime / time);
+            this.gameObject.GetComponent<Transform>().localPosition = new Vector3(xPosition, yValue, zPosition);
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= time)
+            {
+                this.moving = false;
+            }
+            yield return null;
+        }
+    }
+
+    #region Getters and Setters
+    public bool getMachineOn()
+    {
+        return this.machineOn;
+    }
+
+    public void setMachineOn(bool value)
+    {
+        this.machineOn = value;
+    }
+    #endregion
+}
---
+++ SeriousPrototype/Assets/CustomAssets/Scripts/TurnOnBandSaw.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class TurnOnBandSaw : MonoBehaviour
+    {
+        #region Instance Variables
+        [Tooltip("The button to press")]
+        [SerializeField]
+        private HoverButton hoverButton = null;
+
+        [Tooltip("The machine gameobject")]
+        [SerializeField]
+        private GameObject bandSawMachine = null;
+        #endregion
+
+        private void Start()
+        {
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+        }
+
+        private void OnButtonDown(Hand hand)
+        {
+            turnOnMachine();
+        }
+
+        private void turnOnMachine()
+        {
+            BandSawBladeMovement bandSaw = bandSawMachine.GetComponentInChildren<BandSawBladeMovement>();
+            bandSaw.setMachineOn(!bandSaw.getMachineOn());
+        }
+    }
+}
