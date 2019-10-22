57c423114ab485d8eb45fd681617031b88824802
Manas Rawat <manas.rawat@utah.edu>
2019-10-22 08:21:31 -0600

    Added initial pseudo code for the tree snapping script

---
+++ SeriousPrototype/Assets/CustomAssets/Scripts/SnapObjexctToTree.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapObjexctToTree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // DETECT IF A COLLISION IS DETECTED
+        // CHECK IF THE COLLISION IS WITH A CORAL
+        // CHECK WHAT NODE ON THE TREE, THE COLLISION IS WITH
+        // CHECK IF THE CORAL IS RELEASED FROM THE PLAYER'S HAND
+        // SNAP THE CORAL TO THE CAPSULE
+        // TURN OFF THE COLLISION TRIGER FOR THE PARTICULAR CAPSULE
+    }
+}
