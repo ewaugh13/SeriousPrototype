f59bdf68e0d4077c433162d354f9862f0ce5be9a
Manas Rawat <manas.rawat@utah.edu>
2019-10-22 09:15:32 -0600

    Updated snap to tree script to execute with every trigger.

--- SeriousPrototype/Assets/CustomAssets/Scripts/SnapObjexctToTree.cs
+++ SeriousPrototype/Assets/CustomAssets/Scripts/SnapObjexctToTree.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class SnapObjexctToTree : MonoBehaviour
 {
+    #region Instance Variables
+    private bool HasCoral;
+    private float local_x;
+    private float local_y;
+    private float local_z;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HasCoral = false;
+        local_x = this.gameObject.GetComponent<Transform>().localPosition.x;
+        local_y = this.gameObject.GetComponent<Transform>().localPosition.y;
+        local_z = this.gameObject.GetComponent<Transform>().localPosition.z;
     }
+    
+    // Runs whenever a collision is detected in the node's capsule triger
+    private void OnCollisionEnter(Collider other)
+    {
+        // ONLY EXECTUE IF THE NODE HAS NO CORAL
+        if (HasCoral == false)
+        {
+            // CHECK IF THE COLLISION IS WITH A CORAL
+
+            // CHECK IF THE CORAL IS RELEASED FROM THE PLAYER'S HAND
+
+            // SNAP THE CORAL TO THE CAPSULE
+            other.transform.position = transform.position;
+
+            HasCoral = true;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        // DETECT IF A COLLISION IS DETECTED
-        // CHECK IF THE COLLISION IS WITH A CORAL
-        // CHECK WHAT NODE ON THE TREE, THE COLLISION IS WITH
-        // CHECK IF THE CORAL IS RELEASED FROM THE PLAYER'S HAND
-        // SNAP THE CORAL TO THE CAPSULE
-        // TURN OFF THE COLLISION TRIGER FOR THE PARTICULAR CAPSULE
+
     }
 }
