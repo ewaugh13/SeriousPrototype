006d97febf610d60d0f98fa8c4a795b0e0e92a7e
Deep Nath <u1274435@ad.utah.edu>
2019-10-28 17:32:22 -0600

    Completed Deep Scene

---
+++ SeriousPrototype/Assets/CustomAssets/Scripts/CoralDiscCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralDiscCollider : MonoBehaviour
+{
+    public GameObject spawnObject;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Coral")
+        {
+            Debug.Log("Hello");
+
+            Vector3 spawnLocation = new Vector3();
+            spawnLocation.x = collision.gameObject.transform.position.x;
+            spawnLocation.y = collision.gameObject.transform.position.y;
+            spawnLocation.z = collision.gameObject.transform.position.z;
+
+            Quaternion spawnRotation = collision.gameObject.transform.rotation;
+
+            Instantiate(spawnObject, spawnLocation, spawnRotation);
+            Destroy(collision.gameObject);
+        }
+    }
+}
