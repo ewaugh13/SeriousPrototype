65af51aeaacbb7df07dcf1108a6f34123ff362d9
Deep Nath <u1274435@ad.utah.edu>
2019-10-29 17:33:41 -0600

    Applied Underwater Stub Stuff on Dan's Scene

--- SeriousPrototype/Assets/CustomAssets/Scripts/UnderwaterStubs.cs
+++ SeriousPrototype/Assets/CustomAssets/Scripts/UnderwaterStubs.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class UnderwaterStubs : MonoBehaviour
 {
-    public GameObject spawnObject;
+    //public GameObject spawnObject;
 
     public void OnCollisionEnter(Collision collision)
     {
@@ -12,21 +13,31 @@ public class UnderwaterStubs : MonoBehaviour
         {
             //Debug.Log("Hello");
 
-            // Positioning
-            Vector3 spawnLocation = new Vector3();
-            spawnLocation.x = gameObject.transform.position.x;
-            spawnLocation.y = gameObject.transform.position.y + 0.10f;
-            spawnLocation.z = gameObject.transform.position.z;
+            //// Positioning
+            //Vector3 spawnLocation = new Vector3();
+            //spawnLocation.x = gameObject.transform.position.x;
+            //spawnLocation.y = gameObject.transform.position.y + 0.10f;
+            //spawnLocation.z = gameObject.transform.position.z;
 
-            // Rotation
-            Quaternion spawnRotation = Quaternion.Euler(0, 0, 0);
+            //// Rotation
+            //Quaternion spawnRotation = Quaternion.Euler(0, 0, 0);
 
-            // Scale
-            //spawnObject.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+            //// Scale
+            ////spawnObject.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
 
-            // Creation and Destruction
-            Instantiate(spawnObject, spawnLocation, spawnRotation);
-            Destroy(collision.gameObject);
+            //// Creation and Destruction
+            //Instantiate(spawnObject, spawnLocation, spawnRotation);
+            //Destroy(collision.gameObject);
+
+            // Make Parent
+            Destroy(collision.gameObject.GetComponent<Throwable>());
+            Destroy(collision.gameObject.GetComponent<Interactable>());
+
+            collision.gameObject.transform.position = gameObject.transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(gameObject);
+
+            //collision.gameObject.transform.SetParent(gameObject.transform);
         }
     }
 }
