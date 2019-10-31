e9d507f551ebad4aaa93dadd79283dfea3725ed4
Manas Rawat <manas.rawat@utah.edu>
2019-10-30 18:33:14 -0600

    Added UI for teleportation and Button Prompts

---
+++ SeriousPrototype/Assets/CustomAssets/Scripts/DisableStation2TeleportUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableStation2TeleportUI : MonoBehaviour
+{
+    public GameObject UI_Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(UI_Canvas);
+        }
+    }
+}
